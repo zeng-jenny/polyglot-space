c128915ef2b3ac636930899c9fcd4522ebaa3d02
Jenny Zeng <jzeng7077@gmail.com>
2021-01-21 17:15:41 -0500

    Intial setup: Added google cloud vision & translate api

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,30 @@
 const router = require('express').Router()
 module.exports = router
+const vision = require('@google-cloud/vision')
+const {Translate} = require('@google-cloud/translate').v2
+
+async function quickstart() {
+  const client = new vision.ImageAnnotatorClient({
+    keyFilename: './google-secrets.json'
+  })
+  const translate = new Translate({
+    keyFilename: './google-secrets.json'
+  })
+  const [result] = await client.labelDetection(
+    '//Users/jzeng/Grace-Hopper-Program/Stackathon/public/shiba-inu.jpg'
+  )
+  const labels = result.labelAnnotations
+  // console.log('Labels:');
+  const englishArr = labels.map(label => label.description)
+
+  const translatedArr = []
+  for (let i = 0; i < englishArr.length; i++) {
+    const [translation] = await translate.translate(englishArr[i], 'zh-CN')
+    translatedArr.push(translation)
+  }
+  console.log(englishArr, translatedArr)
+}
+quickstart()
 
 router.use('/users', require('./users'))
 
---
+++ server/db/models/photo.js
@@ -0,0 +1,5 @@
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Photo = db.define('photo', {})
