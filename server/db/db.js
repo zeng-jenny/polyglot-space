e64a4b2b80be6627bdaeb79db8c83b5250fffe41
Jenny Zeng <jzeng7077@gmail.com>
2021-01-25 14:15:53 -0500

    added additional db

--- server/db/db.js
+++ server/db/db.js
@@ -9,19 +9,33 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 //     logging: false
 //   }
 // )
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
-    logging: false,
-    operatorsAliases: false,
+
+let db
+if (process.env.DATABASE_URL) {
+  db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    ssl: true,
-    dialectOptions: {
-      ssl: true
-    }
-  }
-)
+    logging: true,
+    ssl: true
+  })
+} else {
+  db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
+    logging: false
+  })
+}
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+//   {
+//     logging: false,
+//     operatorsAliases: false,
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     ssl: true,
+//     dialectOptions: {
+//       ssl: true
+//     }
+//   }
+// )
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
